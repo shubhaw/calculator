9a7c88766827d7e21134b4b0bcf19995812a2f2e
Shubhaw Kumar <shukumar@microsoft.com>
2019-09-09 23:53:17 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Layout from './components/Layout/Layout';
+import CurrencyConverter from './containers/CurrencyConverter/CurrencyConverter';
+import Calculator from './containers/calculator/calculator';
 
 class App extends Component {
-  render () {
-    return <Layout />;
+  render() {
+    return (
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route path="/currencyConverter" component={CurrencyConverter} />
+            <Route path="/" exact component={Calculator} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    );
   }
 }
 
